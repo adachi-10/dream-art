4d3633bccfa9d6d3e4ad021c957cc7c7673e7d85
miekero <adachi1047@gmail.com>
2026-03-19 21:59:25 +0900

    UI入力

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import DreamAnalyzer from "./component/DreamAnalyzer.jsx";
-
-
+import Layout from "./ui/Layout.jsx";
 
 function App() {
   return (
     <div>
+      <Layout/>
       <DreamAnalyzer />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/ui/Layout.jsx
@@ -0,0 +1,7 @@
+export default function HeroSection() {
+  return (
+    <section className="hero">
+      <h1>ゆめをアートで可視化する</h1>
+    </section>
+  );
+}
